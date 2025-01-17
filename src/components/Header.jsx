f5dc4badf4416f26997a8fe3b58ddeb117add895
LuisDCS <1001.27539960.ucla@gmail.com>
2025-01-17 10:27:43 -0400

    Responsive Fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,13 @@ const Header = () => {
       We provide high-quality tinting services for your <br />
       commercial, residential, and automotive needs.
       </p>
-      <video className="header-video" autoPlay muted loop aria-label="Promotional video showcasing our tinting services">
+      <video className="header-video" 
+      autoPlay
+      muted 
+      loop 
+      preload='auto'
+      playsInline
+      aria-label="Promotional video showcasing our tinting services">
         <source src="/video-inicio-1.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
